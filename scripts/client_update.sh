c562c2f63a6257b22cf4919588bebd7171d21c8f
iris-yue-wang <irisluv@gmail.com>
2018-09-10 10:54:12 +0100

    modify: use kubectl command to set new image for rolling update

--- scripts/client_update.sh
+++ scripts/client_update.sh
@@ -4,5 +4,6 @@ cd "${0%/*}"
 
 (cd .. && docker build -t eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-client:v5 .)
 gcloud docker -- push eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-client:v5
-kubectl edit deployment easy-notes-client
+#kubectl edit deployment easy-notes-client
+kubectl set image deployment/easy-notes-client easy-notes-client=eu.gcr.io/zuhlke-kubernetes-codelab/easy-notes-client:v5
 kubectl get deployments
